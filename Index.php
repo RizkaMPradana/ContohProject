958a19ed8bacee7f1462b4eded4ff2d4b3c87a3e
Rizka404 <rizkamuhamad12@gmail.com>
2021-10-29 13:53:24 +0700

    Update Index.php

---
+++ Index.php
@@ -0,0 +1,31 @@
+<?php
+
+$nama = "Nama : Rizka Muhamad Pradana";
+$nim = "NIM : C1983207008";
+$prod = "Prodi : PTI";
+$Sem = "Semester : 5";
+$lahir = "Bekasi, 25 april 2001";
+$tahun1 = "2001";
+$tahun2 = "2021";
+$hobi1 = "Game";
+$hobi2 = "Film";
+$hobi3 = "Musik";
+$hobi4 = "Jejepangan";
+$z=$tahun2-$tahun1;  
+
+echo $nama;
+echo "<br>";
+echo $nim;
+echo "<br>";
+echo $prod;
+echo "<br>";
+echo $Sem;
+echo "<br>";
+echo $lahir;
+echo "<br>";
+echo "umur :", " ", $z, " ", "Tahun";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "Hobi :", " ", $hobi1, ",", $hobi2, ",", $hobi3, ",", $hobi4;
+?>
